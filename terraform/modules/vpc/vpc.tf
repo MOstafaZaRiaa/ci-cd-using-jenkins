3b8ee11d61896fa7721184eb45a84ce9f58ccdf5
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2024-07-10 06:15:44 +0300

    add terrafrom for jenkins

---
+++ terraform/modules/vpc/vpc.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.1.0.0/16"
+   tags = {
+    Name = "cicd-vpc"  
+  }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.1.1.0/24"
+  availability_zone       = var.az  
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id =  aws_vpc.my_vpc.id
+  
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  ingress {
+    description      = "Jenkins-port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+
+  }
+}
